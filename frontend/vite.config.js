560fba068c4c48cdeafa3ac71f39880ba9a69c58
THYWIZARD <abdulhaqabdulganiy@gmail.com>
2025-12-25 12:54:26 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Social_media/'
+  base: '/Social_media/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
